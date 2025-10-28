0e19fdcb38f220840ad3ce0a01b8dc349a9943d5
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-28 10:00:22 +0300

    values show in field edit

--- src/pages/UpdateCourse.jsx
+++ src/pages/UpdateCourse.jsx
@@ -2,24 +2,102 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 import { EditCourse } from "../services/course.js"
+import { ShowCourse } from "../services/course.js"
 
 import SendIcon from "@mui/icons-material/Send"
+import Button from "@mui/material/Button"
 import TextField from "@mui/material/TextField"
 
 const UpdateCourse = () => {
   const { courseId } = useParams()
+  let navigate = useNavigate()
 
-  const [values, setValues] = useState([])
+  const initialState = {
+    title: "",
+    description: "",
+    objective: "",
+    level: "",
+    category: "",
+  }
+  const [formValues, setFormValues] = useState(initialState)
+
   useEffect(() => {
     const updateCourse = async () => {
-      const data = await EditCourse(courseId)
-      setValues(data)
+      const data = await ShowCourse(courseId)
+      setFormValues(data)
     }
     updateCourse()
   }, [courseId])
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await EditCourse(formValues)
+    setFormValues(initialState)
+    navigate("/")
+  }
+
   return (
     <>
-      <h1>Update</h1>
+      <div className="add-course-container">
+        <h1>Add a New Course</h1>
+        <form onSubmit={handleSubmit} className="add-course-form">
+          <TextField
+            required
+            label="Course Title"
+            placeholder="Enter a clear course title"
+            name="title"
+            type="text"
+            onChange={handleChange}
+            value={formValues.title}
+          />
+          <TextField
+            required
+            label="Description"
+            placeholder="Provide a brief overview of the course content and outcomes"
+            name="description"
+            type="text"
+            multiline
+            rows={4}
+            onChange={handleChange}
+            value={formValues.description}
+          />
+          <TextField
+            required
+            label="Objectives"
+            placeholder="State the main goals of this course"
+            name="objective"
+            type="text"
+            multiline
+            rows={2}
+            onChange={handleChange}
+            value={formValues.objective}
+          />
+          <TextField
+            required
+            label="Level"
+            placeholder="e.g. Beginner, Intermediate, Advanced"
+            name="level"
+            type="text"
+            onChange={handleChange}
+            value={formValues.level}
+          />
+          <TextField
+            required
+            label="Category"
+            placeholder="Enter a category"
+            name="category"
+            type="text"
+            onChange={handleChange}
+            value={formValues.category}
+          />
+          <Button variant="contained" endIcon={<SendIcon />} type="submit">
+            Update Course
+          </Button>
+        </form>
+      </div>
     </>
   )
 }
